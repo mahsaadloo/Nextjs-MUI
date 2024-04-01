53ab41a7ff904f26717ac01fea7a193e23ac75fe
mahsa <mahsaadlooo@gmail.com>
2024-04-01 14:04:18 -0700

    fix: :bug: fixed an indent on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Link from 'next/link'
-import ProductCard from './components/ProductCard'
-import { Typography } from '@mui/material'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
+import { Typography } from "@mui/material";
 
 export default function Home() {
   return (
     <main>
-      <Typography variant='h1'> Hello </Typography>
+      <Typography variant="h1"> Hello </Typography>
       <h1>Hello world!</h1>
       <Link href="/users">users</Link>
       <ProductCard />
     </main>
-  )
+  );
 }
